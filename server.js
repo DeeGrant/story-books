782b362463999d73ec74eb39c8500972cf56b927
Dee Grant <WalterGrant@gmail.com>
2022-01-07 15:10:30 -0600

    connect to db

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 dotenv.config({path: 'config/.env'})
+
+connectDB()
+
 const app = express()
 
 const PORT = process.env.PORT || 8000
