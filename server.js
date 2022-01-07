9f70fe3f3308e45351321d8844529c87f3e991aa
Dee Grant <WalterGrant@gmail.com>
2022-01-07 16:47:34 -0600

    add cors

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -16,6 +17,7 @@ const app = express()
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+app.use(cors())
 
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
